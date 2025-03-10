aa4a9108fdf8971689ace758b18c7257216da763
Prashant.patil <prashant.patil@hyd-prashant.pa>
2025-03-10 16:17:46 +0530

    stringify the obj

--- src/script.ts
+++ src/script.ts
@@ -253,7 +253,7 @@ function setupThoughtSpotEmbed(typeofEmbed: string, viewConfig: Record<string, a
     }
 
   let embedInstance: LiveboardEmbed | SearchEmbed | null = null;
-    alert(` This is the final config ${validateAndMergeViewConfig(viewConfig)}`);
+    alert(` This is the final config ${JSON.stringify(validateAndMergeViewConfig(viewConfig))}`);
     if (typeofEmbed === "Liveboard") {
         embedInstance = new LiveboardEmbed("#ts-embed", {
             ...validateAndMergeViewConfig(viewConfig),
